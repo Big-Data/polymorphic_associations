ab7426b9ae78d812bab7824a383ba52c2b39cb34
Rico Marcelo <rico.c.marcelo@gmail.com>
2012-03-09 20:14:08 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PolymorphicAssociations::Application.config.secret_token = '9633e53f04b22658efbdb3b7f072ef96f8eb2e8eea7bf25a92b3e37782f75386de60e74357f8628f8bcedcd70255a06f331c47f6c574686e030d9b898b72a6ad'
